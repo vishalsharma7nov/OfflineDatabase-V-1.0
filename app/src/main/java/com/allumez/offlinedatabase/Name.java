7f51484e7a1abab9a911410033dba6bb6462270f
home <vishalsharma7nov@gmail.com>
2019-04-30 17:21:21 +0530

    Online Db connectivity not working

---
+++ app/src/main/java/com/allumez/offlinedatabase/Name.java
@@ -0,0 +1,19 @@
+package com.allumez.offlinedatabase;
+
+public class Name {
+    private String name;
+    private int status;
+
+    public Name(String name, int status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
